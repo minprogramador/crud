569356fb4ffbc6796f102b06829f9ab7c6c132b3
putty <minprogramador@gmail.com>
2022-04-09 20:32:36 -0300

    form submit ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [values, setValues] = useState();
+  
+  const handleChangeValues = value => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name]: value.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
+  };
+
   return (
     <div className="app--container">
       <div className='register--container'>
         <h1 className='register--title'>Scrim Shop</h1>
 
-        <input type="text" name="nome" placeholder="Nome" className="register--input"/>
-        <input type="text" name="preco" placeholder="Preco" className="register--input"/>
-        <input type="text" name="categoria" placeholder="Categoria" className="register--input"/>
-        <button className='register--button'>Cadastrar</button>
+        <input type="text" name="nome" placeholder="Nome" className="register--input" onChange={handleChangeValues}/>
+        <input type="text" name="preco" placeholder="Preco" className="register--input"onChange={handleChangeValues}/>
+        <input type="text" name="categoria" placeholder="Categoria" className="register--input"onChange={handleChangeValues}/>
+        <button className='register--button' onClick={()=> {handleClickButton()}}>Cadastrar</button>
       </div>
     </div>
   );
