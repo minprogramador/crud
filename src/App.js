03d42dd9688fc143c2e4a7d15dc175c0b2347f86
putty <minprogramador@gmail.com>
2022-04-09 20:25:34 -0300

    form e css

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app--container">
+      <div className='register--container'>
+        <h1 className='register--title'>Scrim Shop</h1>
+
+        <input type="text" name="nome" placeholder="Nome" className="register--input"/>
+        <input type="text" name="preco" placeholder="Preco" className="register--input"/>
+        <input type="text" name="categoria" placeholder="Categoria" className="register--input"/>
+        <button className='register--button'>Cadastrar</button>
+      </div>
     </div>
   );
 }
